45cde0eea3e27bf295762bfe5272976e5b84f650
Morgan Edmonds <morgan.edmonds@willowtreeapps.com>
2021-03-06 17:40:41 -0500

    Build out basic UI

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .kerning(2.0)
+                .bold()
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+            Text("89")
+                .kerning(-1.0)
+                .fontWeight(.black)
+                .font(.largeTitle)
+            HStack {
+                Text("1")
+                    .bold()
+                Slider(value: .constant(50), in: 1.0...100.0)
+                Text("100")
+                    .bold()
+            }
+            Button(action: {}) {
+                Text("Hit me!")
+                    .bold()
+                    .multilineTextAlignment(.center)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView().previewLayout(.fixed(width: 568, height: 320))
     }
 }
