32228cbbdf5f22d6c5f7e2ee49978837de0a7c69
Morgan Duverney <morgan.duverney@teachstone.com>
2021-04-10 14:51:56 -0400

    Implement points method and relevant tests

---
+++ BullseyeTests/BullseyeTests.swift
@@ -0,0 +1,35 @@
+//
+//  BullseyeTests.swift
+//  BullseyeTests
+//
+//  Created by Morgan Duverney on 4/10/21.
+//
+
+import XCTest
+@testable import Bullseye
+
+class BullseyeTests: XCTestCase {
+    
+    var game: Game!
+
+    override func setUpWithError() throws {
+        game = Game()
+    }
+
+    override func tearDownWithError() throws {
+        game = nil
+    }
+
+    func testScorePositive() throws {
+        let guess = game.target + 5
+        let score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 95)
+    }
+    
+    func testScoreNegative() throws {
+        let guess = game.target - 5
+        let score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 95)
+    }
+
+}
